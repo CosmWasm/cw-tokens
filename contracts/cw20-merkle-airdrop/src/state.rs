a0feb7330dd286768879a62ba28076625b0bee60
Orkun Külçe <orkunkl@users.noreply.github.com>
2021-12-28 22:14:40 +0300

    Port cw20-merkle-airdrop expire and start changes (#5)

--- contracts/cw20-merkle-airdrop/src/state.rs
+++ contracts/cw20-merkle-airdrop/src/state.rs
@@ -3,6 +3,7 @@ use serde::{Deserialize, Serialize};
 
 use cosmwasm_std::Addr;
 use cw_storage_plus::{Item, Map};
+use cw_utils::{Expiration, Scheduled};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct Config {
@@ -16,6 +17,12 @@ pub const CONFIG: Item<Config> = Item::new(CONFIG_KEY);
 
 pub const LATEST_STAGE_KEY: &str = "stage";
 pub const LATEST_STAGE: Item<u8> = Item::new(LATEST_STAGE_KEY);
+
+pub const STAGE_EXPIRATION_KEY: &str = "stage_exp";
+pub const STAGE_EXPIRATION: Map<u8, Expiration> = Map::new(STAGE_EXPIRATION_KEY);
+
+pub const STAGE_START_KEY: &str = "stage_start";
+pub const STAGE_START: Map<u8, Scheduled> = Map::new(STAGE_START_KEY);
 
 pub const MERKLE_ROOT_PREFIX: &str = "merkle_root";
 pub const MERKLE_ROOT: Map<u8, String> = Map::new(MERKLE_ROOT_PREFIX);
