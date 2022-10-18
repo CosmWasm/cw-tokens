8dcd5f7d9151228640742f1a6212fed0ceaa4c9e
Le Turt <89463679+LeTurt333@users.noreply.github.com>
2022-10-18 00:00:24 -0500

    fix issues 2

--- contracts/cw20-receiver-minimal/src/contract.rs
+++ contracts/cw20-receiver-minimal/src/contract.rs
@@ -18,6 +18,7 @@ const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
     deps: DepsMut,
+    _env: Env,
     info: MessageInfo,
     msg: InstantiateMsg,
 ) -> Result<Response, ContractError> {
@@ -45,6 +46,7 @@ pub fn instantiate(
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn execute(
     deps: DepsMut,
+    _env: Env,
     info: MessageInfo,
     msg: ExecuteMsg,
 ) -> Result<Response, ContractError> {
